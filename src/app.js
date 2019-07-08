1ef58abc6206b48f3195c632a608476bbf7dfea7
J. Randall Hunt <randallhunt@gmail.com>
2019-07-08 01:31:19 -0700

    working javascript - also wtf javascript

--- src/app.js
+++ src/app.js
@@ -1,29 +1,110 @@
-const apigw = "https://pvli4gp5c8.execute-api.us-east-1.amazonaws.com/prod/streams/"
-const clickstream = apigw + "ClickStream/record"
+/* CHANGE ALL OF THESE BASED ON THE CLOUDFORMATION OUTPUT */
+const APIBaseURL = "https://1w7f57jh5d.execute-api.us-east-1.amazonaws.com/prod/streams/"
+const hotspotStream = "deleteme2-HotSpotStream-DO7K55940CZ0"
+const clickStream = "deleteme2-ClickStream-13F0KD49I1136"
 
-function recordClickEvent(canvas, event) {
+// Don't change these
+const clickstreamURL = new URL(APIBaseURL + clickStream + "/record")
+const hostpotIteratorURL = new URL(APIBaseURL + hotspotStream + "/sharditerator")
+const hotspotRecordsURL = new URL(APIBaseURL + hotspotStream + "/record")
+const hotspotURL = new URL(APIBaseURL + hotspotStream + "/")
+
+var shards = new Map()
+
+async function getShards() {
+    const response = await fetch(hotspotURL)
+    const json = await response.json()
+    json.StreamDescription.Shards.forEach(async (element) => {
+        shards.set(element.ShardId, await getShardIterator(element.ShardId))
+    })
+}
+
+async function getShardIterator(shard) {
+    const response = await fetch(hostpotIteratorURL + "?shard-id=" + encodeURIComponent(shard))
+    const json = await response.json()
+    return json.ShardIterator
+}
+
+async function getHotspots(iterator) {
+    const response = await fetch(
+        hotspotRecordsURL,
+        {headers: new Headers({'Shard-Iterator': iterator})}
+    )
+    const json = await response.json()
+    return json
+}
+
+async function paintHotspots(hotspots) {
+    hotspots.forEach(hotspot => {
+        context.beginPath()
+        if (hotspot.density != "Infinity") {
+            context.lineWidth = hotspot.density * 100
+        } else {
+            context.lineWidth = 10
+        }
+        context.strokeStyle = "red"
+        context.rect(
+            hotspot.minValues[0],
+            hotspot.minValues[1],
+            hotspot.maxValues[0] - hotspot.minValues[0],
+            hotspot.maxValues[1] - hotspot.minValues[1]
+        )
+        context.stroke()
+    })
+}
+
+async function recordClickEvent(canvas, event) {
     const rect = canvas.getBoundingClientRect()
     const x = Math.floor(event.clientX - rect.left)
     const y = Math.floor(event.clientY - rect.top)
-    console.log("x: " + x + " y: " + y)
-    const request = async () => {
-        const response = await fetch(
-            clickstream, {
-                headers: new Headers({
-                    'Content-Type': 'application/json'
-                }),
-                method: 'PUT',
-                body: JSON.stringify({"x": x, "y": y})
-            }
-        )
-        const json = await response.json()
-        console.log(json)
-    }
-    request();
+    console.log(x, y)
+    const response = await fetch(clickstreamURL,
+        {
+            headers: new Headers({'Content-Type': 'application/json'}),
+            method: 'PUT',
+            body: JSON.stringify({"x": x, "y": y})
+        }
+    )
 }
 
 const canvas = document.querySelector('#app-canvas')
-canvas.addEventListener('click', function(e) {
-    e.preventDefault()
-    recordClickEvent(canvas, e)
+
+/* JAVASCRIPT IS FUCKING CRAZY */
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+const context = canvas.getContext('2d')
+
+/* every time someone clicks, shoot the x,y coords over to kinesis */
+canvas.addEventListener('click', async event => {
+    event.preventDefault()
+    await recordClickEvent(canvas, event)
 })
+
+/* start by building a map of shards and shard iterators */
+const start = async () => {
+    await getShards()
+}
+start()
+
+/* every 5 minutes grab new shard iterators */
+setInterval(async () => {
+    await getShards()
+}, 300000)
+
+/* every 4 seconds or so:
+1. Grab all the shard iterators
+2. Fetch new records from them
+3. parse the resulting base64 encoded records into JSON and then each row into JSON
+4. throw it over to our paint job to make little rectangles
+*/
+setInterval(async () => {
+    shards.forEach(async (value, key, map) => {
+        const hotspotsData = await getHotspots(value)
+        hotspotsData.Records.forEach(async record => {
+            const hotspots = JSON.parse(JSON.parse(atob(record.Data)).HOTSPOTS_RESULT).hotspots
+            console.log(hotspots)
+            await paintHotspots(hotspots)
+        })
+    })
+}, 4000)
